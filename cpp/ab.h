e6156dd0b5c03f1ed22a5d9734540df63fe3e2d4
Franco Frizzo <francofrizzo@gmail.com>
2015-07-01 22:56:56 -0300

    De a poquito va avanzando el árbol

--- cpp/ab.h
+++ cpp/ab.h
@@ -2,8 +2,11 @@
 #define ARBOL_BIN_H
 
 #include <algorithm>
+#include <iostream>
 #include "./aed2.h"
 
+using std::cerr;
+using std::endl;
 using std::max;
 using namespace aed2;
 
@@ -12,29 +15,51 @@ namespace tp3 {
 template<class T>
 class ab {
  private:
-    bool _esNil;
-    T valor;
-    ab<T>* _izq;
-    ab<T>* _der;
+    struct nodo {
+        T valor;
+        ab<T>* _izq;
+        ab<T>* _der;
+
+        nodo(ab<T>* i, T v, ab<T>* d) :
+            valor(v),
+            _izq(i),
+            _der(d) {}
+
+        nodo(const nodo& otro) :
+            valor(otro.valor) {
+            if (otro._izq != NULL) {
+                _izq = new ab<T>(*(otro._izq));
+            } else {
+                _izq = NULL;
+            }
+            if (otro._der != NULL) {
+                _der = new ab<T>(*(otro._der));
+            } else {
+                _der = NULL;
+            }
+        }
+
+        ~nodo() {
+            delete _izq;
+            delete _der;
+        }
+    };
+
+    nodo* _raiz;
 
  public:
     ab();  // Constructor vacío (Nil)
     ab(const ab<T>&);  // Constructor por copia
-
-    // Variantes del constructor Bin:
-    ab(const T&);
-    ab(ab<T>&, const T&);
-    ab(const T&, ab<T>&);
-    ab(ab<T>&, const T&, ab<T>&);
+    ab(ab<T>*, const T&, ab<T>*);  // Constructor Bin
 
     ~ab();
 
-    ab<T>& operator=(const ab<T>&); // Operador de asignación
+    ab<T>& operator=(const ab<T>&);  // Operador de asignación por copia
 
     bool esNil() const;  // Devuelve True si y solo si el árbol no contiene nodos
     T& raiz() const;  // Devuelve el valor de la raíz del árbol
-    ab<T>& izq() const;  // Devuelve el subárbol izquierdo
-    ab<T>& der() const;  // Devuelve el subárbol derecho
+    ab<T>* izq() const;  // Devuelve el subárbol izquierdo
+    ab<T>* der() const;  // Devuelve el subárbol derecho
     Nat altura() const;  // Devuelve la altura del árbol
     Nat cantNodos() const;  // Devuelve la cantidad de nodos del árbol
 };
@@ -45,40 +70,36 @@ template<class T>
 ab<T>::ab() : _raiz(NULL) {}
 
 template<class T>
-ab<T>::ab(const ab<T>& otro) : _raiz(otro._raiz) {}
-
-template<class T>
-ab<T>::ab(const T& r) {
-    ab<T>* i = new ab<T>();
-    ab<T>* d = new ab<T>();
-    _raiz = new subarbol(*i, r, *d);
+ab<T>::ab(const ab<T>& otro) {
+    *this = otro;
 }
 
 template<class T>
-ab<T>::ab(ab<T>& i, const T& r) {
-    ab<T>* d = new ab<T>();
-    _raiz = new subarbol(i, r, *d);
-}
-
-template<class T>
-ab<T>::ab(const T& r, ab<T>& d) {
-    ab<T>* i = new ab<T>();
-    _raiz = new subarbol(*i, r, d);
-}
-
-template<class T>
-ab<T>::ab(ab<T>& i, const T& r, ab<T>& d) {
-    _raiz = new subarbol(i, r, d);
+ab<T>::ab(ab<T>* i, const T& r, ab<T>* d) {
+    _raiz = new nodo(i, r, d);
 }
 
 template<class T>
 ab<T>::~ab() {
-    delete _raiz;
+    // delete _raiz;
 }
 
 template<class T>
 ab<T>& ab<T>::operator=(const ab<T>& otro) {
-    _raiz = otro._raiz;
+    delete _raiz;
+    _raiz = NULL;
+    if (otro._raiz != NULL) {
+        ab<T>* izquierda, * derecha;
+        if (otro._raiz->_izq != NULL) {
+            izquierda = new ab<T>(*(otro._raiz->_izq));
+        }
+        if (otro._raiz->_der != NULL) {
+            derecha = new ab<T>(*(otro._raiz->_der));
+        }
+        _raiz = new nodo(izquierda, otro._raiz->valor, derecha);
+    } else {
+        _raiz = NULL;
+    }
     return *this;
 }
 
@@ -94,15 +115,15 @@ T& ab<T>::raiz() const {
 }
 
 template<class T>
-ab<T>& ab<T>::izq() const {
+ab<T>* ab<T>::izq() const {
     assert(!esNil());
-    return *(_raiz->izq);
+    return _raiz->_izq;
 }
 
 template<class T>
-ab<T>& ab<T>::der() const {
+ab<T>* ab<T>::der() const {
     assert(!esNil());
-    return *(_raiz->der);
+    return _raiz->_der;
 }
 
 template<class T>
@@ -110,7 +131,7 @@ Nat ab<T>::altura() const {
     if (esNil()) {
         return 0;
     } else {
-        return 1 + max(izq().altura(), der().altura());
+        return 1 + max(izq()->altura(), der()->altura());
     }
 }
 
@@ -119,7 +140,7 @@ Nat ab<T>::cantNodos() const {
     if (esNil()) {
         return 0;
     } else {
-        return 1 + izq().cantNodos() + der().cantNodos();
+        return 1 + izq()->cantNodos() + der()->cantNodos();
     }
 }
 
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 // #include "./aed2.h"
 // #include "./dicc_trie.h"
-// #include "./ab.h"
+#include "./ab.h"
 // #include "./dicc_log.h"
 // #include "./cola_prior.h"
 // #include "./red.h"
-#include "./dcnet.h"
+// #include "./dcnet.h"
+#include "./mini_test.h"
 
 using namespace aed2;
 using namespace tp3;
@@ -14,25 +15,47 @@ using std::cout;
 using std::endl;
 
 void test_ab() {
-    // ab<int> nil1 = ab<int>();
-    // ab<int> nil2 = ab<int>();
-    // ab<int> nil3 = ab<int>();
-    // ab<int> nil4 = ab<int>();
-    // ab<int> ii = ab<int>(nil1, 1, nil2);
-    // ab<int> dd = ab<int>(nil3, 4, nil4);
-    // ab<int> cc = ab<int>(ii, 8, dd);
+    ab<int>* nil1 = new ab<int>();
+    ab<int>* nil2 = new ab<int>();
+    ab<int>* nil3 = new ab<int>();
+    ab<int>* nil4 = new ab<int>();
+    ab<int>* I = new ab<int>(nil1, 1, nil2);
+    ab<int>* D = new ab<int>(nil3, 4, nil4);
+    ab<int>* C = new ab<int>(I, 8, D);
+
+    ASSERT_EQ(C->raiz(), 8);
+    ASSERT_EQ(C->izq()->raiz(), 1);
+    ASSERT_EQ(C->der()->raiz(), 4);
+    ASSERT_EQ(C->altura(), 2);
+    ASSERT_EQ(C->cantNodos(), 3);
+
+    nil1 = new ab<int>();
+    nil2 = new ab<int>();
+    ab<int>* A = new ab<int>(nil1, 16, nil2);
+
+    ASSERT_EQ(A->raiz(), 16);
+    ASSERT_EQ(A->altura(), 1);
+    ASSERT_EQ(A->cantNodos(), 1);
+
+    *A = *C;
+
+    ASSERT_EQ(A->raiz(), 8);
+
+    delete(C);
+    delete(A);
+
     // cout << cc.raiz() << endl;
     // cout << "Altura de cc: " << cc.altura() << endl;
     // cout << "Cant de nodos de cc: " << cc.cantNodos() << endl;
     // ab<int> nil5 = ab<int>();
     // ab<int> nil6 = ab<int>();
-    // cc.izq().izq() = ab<int>(nil5, 6, nil6);
+    // cc.izq()->izq() = ab<int>(nil5, 6, nil6);
     // cout << "Altura de cc: " << cc.altura() << endl;
     // cout << "Cant de nodos de cc: " << cc.cantNodos() << endl;
     // ab<int> nuevo = cc;
-    // cout << nuevo.izq().raiz() << endl;
-    // cc.izq().raiz() = 80;
-    // cout << nuevo.izq().raiz() << endl;
+    // cout << nuevo.izq()->raiz() << endl;
+    // cc.izq()->raiz() = 80;
+    // cout << nuevo.izq()->raiz() << endl;
 }
 
 void test_dicc_log() {
@@ -74,11 +97,7 @@ void test_heap() {
 }
 
 int main() {
-
-    // test_red();
-    // test_ab();
-    // test_dicc_log();
-    // test_heap();
+    RUN_TEST(test_ab);
     
     return 0;
 }
